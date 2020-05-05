d9b603eaf03bcb7c0436fde7e470975577464b69
TeBeCo <tebeco@gmail.com>
2020-05-05 19:43:13 +0200

    create emprty JobController

---
+++ src/KickingBgJobs/Controllers/JobController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace KickingBgJobs.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JobController : ControllerBase
+    {
+        private readonly ILogger<JobController> _logger;
+
+        public JobController(ILogger<JobController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
